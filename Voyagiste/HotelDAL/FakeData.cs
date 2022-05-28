f254a09cb9e5068781461f71a1cf1083844040c2
NicolasSt-Arnault <2031116@etu.cegepsth.qc.ca>
2022-05-28 11:14:11 -0400

    Début de la correction de fake data

--- Voyagiste/HotelDAL/FakeData.cs
+++ Voyagiste/HotelDAL/FakeData.cs
@@ -6,88 +6,106 @@ namespace HotelDAL
     public class FakeData
     {
         private static FakeData? Singleton;
+        internal List<HotelBooking>? hotelBookings;
+        internal List<Hotel>? hotels;
+        //À compléter ici les list
+        #region Titles
+        internal static readonly Title[] titles = {
+            new Title("M."),
+            new Title("Mme")
+        };
+        #endregion
 
-        private FakeData()
-        {
-            #region Titles
-            var titles = new List<Title>();
-            titles.Add(new Title("M."));
-            titles.Add(new Title("Mme"));
-            #endregion
-            
-            #region BirthDates
-            var birthDates = new List<BirthDate>();
-            birthDates.Add(new BirthDate(new DateTime (2000, 1, 1)));
-            birthDates.Add(new BirthDate(new DateTime (2003, 5, 1)));
-            #endregion
-            
-            #region Countries
-            var countries = new List<Country>();
-            countries.Add(new Country("Canada"));
-            countries.Add(new Country("France"));
-            #endregion
-          
-            #region Regions
-            var regions = new List<Region>();
-            regions.Add(new Region("Oussa"));
-            regions.Add(new Region("Saitou"));
-            regions.Add(new Region("Somewhere"));
+        #region BirthDates
+        internal static readonly BirthDate[] birthDates = {
+            new BirthDate(new DateTime(2000, 1, 1)),
+            new BirthDate(new DateTime(2003, 5, 1))
+        };
+        #endregion
+
+        #region Countries
+        internal static readonly Country[] countries = {
+        new Country("Canada"),
+            new Country("France")
+
+        };
+        #endregion
+
+        #region Regions
+        internal static readonly Region[] regions = {
+        new Region("Oussa"),
+            new Region("Saitou"),
+            new Region("Somewhere")
+        };
             #endregion
 
             #region Cities
-            var cities = new List<City>();
-            cities.Add(new City("Laval"));
-            cities.Add(new City("Marseille"));
-            #endregion
+        internal static readonly City[] cities = {
+            new City("Laval"),
+            new City("Marseille")
+            };
+        #endregion
 
-            #region PostalCodes
-            var postalCodes = new List<PostalCode>();
-            postalCodes.Add(new PostalCode("B3RR3B"));
-            postalCodes.Add(new PostalCode("D4SS4D"));
-            postalCodes.Add(new PostalCode("F5TT5F"));
-            #endregion
+        #region PostalCodes
+        internal static readonly PostalCode[] postalCodes = { 
+        new PostalCode("B3RR3B"),
+         new PostalCode("D4SS4D"),
+         new PostalCode("F5TT5F")
+        };
+        #endregion
 
-            #region Passports
-            var passports = new List<Passport>();
-            passports.Add(new Passport(new Guid("5e8fea4d-9f74-42fc-9556-03cfecc30849"), countries[0], "12CS12345", new DateTime(2033, 5, 13)));
-            passports.Add(new Passport(new Guid("bbf643ec-cf56-4e96-a15a-2da78999d553"), countries[0], "13AM20035", new DateTime (2036, 10, 14)));
-            #endregion
-            
-            #region Guests (Persons)
-            var guests = new List<Person>();
-            guests.Add(new Person(new Guid("37275592-4c5b-4d16-a1a2-5ffac17ffd15"), titles[0], "Cassian", "Stan", "Scarlet", birthDates[0], passports[0]));
-            guests.Add(new Person(new Guid("b184de97-d0be-413a-b4f5-bbdba601e9c2"), null, "Kaimi", null, "Inanis", null, null));
-            guests.Add(new Person(new Guid("40bda044-9891-4c3a-8dde-fc8133590405"), titles[1], "Angelica", "Maria", "Marika", birthDates[1], passports[1]));
-            #endregion
+        #region Passports
+        internal static readonly Passport[] passports = { 
+        new Passport(new Guid("5e8fea4d-9f74-42fc-9556-03cfecc30849"), countries[0], "12CS12345", new DateTime(2033, 5, 13)),
+        new Passport(new Guid("bbf643ec-cf56-4e96-a15a-2da78999d553"), countries[1], "13AM20035", new DateTime(2036, 10, 14))
+        };
+        #endregion
 
-            #region Addresses
-            var addresses = new List<Address>();
-            addresses.Add(new Address(new Guid("6e746e0d-1b8d-494a-9359-34bfc155f58f"), countries[0], regions[0], cities[0], postalCodes[0], "404, rue Chezpasou"));
-            addresses.Add(new Address(new Guid("72f403bd-3906-4928-a384-c815e6131f93"), countries[0], regions[1], cities[1], postalCodes[1], "405, rue Chuiou"));
-            addresses.Add(new Address(new Guid("639137dc-650c-4f09-96a0-67573aa6e05d"), countries[1], regions[2], cities[2], postalCodes[2], "505, rue Chuiperdu"));
-            #endregion
+        #region Guests (Persons)
+        internal static readonly Person[] guests = { 
+        new Person(new Guid("37275592-4c5b-4d16-a1a2-5ffac17ffd15"), titles[0], "Cassian", "Stan", "Scarlet", birthDates[0], passports[0]),
+            new Person(new Guid("b184de97-d0be-413a-b4f5-bbdba601e9c2"), null, "Kaimi", null, "Inanis", null, null),
+            new Person(new Guid("40bda044-9891-4c3a-8dde-fc8133590405"), titles[1], "Angelica", "Maria", "Marika", birthDates[1], passports[1])
+        };
+        #endregion
 
-            #region Hotels
-            var hotels = new List<Hotel>();
-            hotels.Add(new Hotel(new Guid("54973543-6cf3-4c88-8b47-f48d87bcf392"), addresses[0]));
-            hotels.Add(new Hotel(new Guid("d223bea0-a58a-46dd-8463-0c9727d33071"), addresses[1]));
-            hotels.Add(new Hotel(new Guid("1fa4aa21-d1fb-417b-b207-59d219b1a582"), addresses[2]));
+        #region Addresses
+        internal static readonly Address[] addresses =
+        {
 
-            #endregion
+        new Address(new Guid("6e746e0d-1b8d-494a-9359-34bfc155f58f"), countries[0], regions[0], cities[0], postalCodes[0], "404, rue Chezpasou"),
+           new Address(new Guid("72f403bd-3906-4928-a384-c815e6131f93"), countries[0], regions[1], cities[1], postalCodes[1], "405, rue Chuiou"),
+           new Address(new Guid("639137dc-650c-4f09-96a0-67573aa6e05d"), countries[1], regions[2], cities[2], postalCodes[2], "505, rue Chuiperdu")
+        };
+        #endregion
 
-            #region Rooms
-            var rooms = new List<Room>();
-            rooms.Add(new Room(new Guid("207602ae-fb7f-4e8f-954e-c906b37bd40d"), hotels[0], "Chambre101"));
-            rooms.Add(new Room(new Guid("e5f4bb57-b6aa-4c64-932c-d54a0a860dbe"), hotels[1], "Chambre202"));
-            rooms.Add(new Room(new Guid("f98befd5-7a4a-44bb-9043-1090ad405644"), hotels[2], "Chambre303"));
-            #endregion
+        #region Hotels
+        internal static readonly Hotel[] hotels = { 
+        new Hotel(new Guid("54973543-6cf3-4c88-8b47-f48d87bcf392"), addresses[0]),
+        new Hotel(new Guid("d223bea0-a58a-46dd-8463-0c9727d33071"), addresses[1]),
+        new Hotel(new Guid("1fa4aa21-d1fb-417b-b207-59d219b1a582"), addresses[2])
+        };
 
-            #region HotelBookings
-            var hotelBookings = new List<HotelBooking>();
-            hotelBookings.Add(new HotelBooking(new Guid("d3062793-8a01-4e8a-beba-2db3a9cf0871"), rooms[0], guests[0], new DateTime(2022, 5, 11)));
-            hotelBookings.Add(new HotelBooking(new Guid("c901d3e7-17ee-44f8-8787-842a5e72184f"), rooms[1], guests[1], new DateTime(2023, 10, 12)));
-            hotelBookings.Add(new HotelBooking(new Guid("52b54352-7415-4723-bc11-5caebaba9f8c"), rooms[2], guests[3], new DateTime(2024, 12, 13)));
+        #endregion
+
+        #region Rooms
+        internal static readonly Room[] rooms = { 
+        new Room(new Guid("207602ae-fb7f-4e8f-954e-c906b37bd40d"), hotels[0], "Chambre101"),
+        new Room(new Guid("e5f4bb57-b6aa-4c64-932c-d54a0a860dbe"), hotels[1], "Chambre202"),
+        new Room(new Guid("f98befd5-7a4a-44bb-9043-1090ad405644"), hotels[2], "Chambre303")
+        };
+        #endregion
+
+
+        #region HotelBookings
+        internal static readonly HotelBooking[] hotelBookings = { 
+        new HotelBooking(new Guid("d3062793-8a01-4e8a-beba-2db3a9cf0871"), rooms[0], guests[0], new DateTime(2022, 5, 11)),
+        new HotelBooking(new Guid("c901d3e7-17ee-44f8-8787-842a5e72184f"), rooms[1], guests[1], new DateTime(2023, 10, 12)),
+        new HotelBooking(new Guid("52b54352-7415-4723-bc11-5caebaba9f8c"), rooms[2], guests[3], new DateTime(2024, 12, 13))
+        };
             #endregion
+        private FakeData()
+        {
         }
         internal static FakeData GetInstance()
         {
--- Voyagiste/HotelDAL/HotelDataAccess.cs
+++ Voyagiste/HotelDAL/HotelDataAccess.cs
@@ -21,12 +21,108 @@ namespace HotelDAL
         public HotelBooking? GetHotelBooking(Guid HotelBookingId);
         public HotelBooking[] GetHotelBookings(Person rentedTo);
         public HotelBooking[] GetHotelBookings(Hotel hotel);
-        public HotelBooking Book(Hotel hotel, DateTime From, DateTime To, Person rentedTo);
+        public HotelBooking Book(Room room, Person guest, DateTime bookedWhen);
 
         public BookingConfirmation ConfirmBooking(HotelBooking booking);
         public BookingConfirmation? GetBookingConfirmation(HotelBooking booking);
 
         public BookingCancellation CancelBooking(HotelBooking booking);
         public BookingCancellation? GetBookingCancellation(HotelBooking booking);
+    }
+
+    public class HotelDataAccess : IHotelDataAccess
+    {
+        private IConfiguration _configuration;
+        private ILogger _logger;
+
+        public HotelDataAccess(IConfiguration configuration, ILogger<HotelDataAccess> logger)
+        {
+            _configuration = configuration; // Permet éventuellement de recevoir ici la connexion string pour la base de données
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Enregistre une réservation de voiture
+        /// 
+        /// Attention, les disponibilités ne sont pas gérés par le DAL
+        /// </summary>
+        public HotelBooking Book(Room room, Person guest, DateTime bookedWhen)
+        {
+            var booking = new HotelBooking(new Guid(), room, guest, bookedWhen);
+            FakeData.GetInstance().hotelBookings.Add(booking);
+            return booking;
+        }
+
+        /// <summary>
+        /// Enregistre une cancellation de voiture
+        /// 
+        /// Attention, la gestion du retrait de booking et les remises en disponibilités ne sont pas gérés par le DAL
+        /// </summary>
+        public BookingCancellation CancelBooking(HotelBooking booking)
+        {
+            BookingCancellation bc = new BookingCancellation(new Guid(), booking, new DateTime());
+            FakeData.GetInstance().bookingCancellations.Add(bc);
+            return bc;
+        }
+
+        public BookingConfirmation ConfirmBooking(HotelBooking booking)
+        {
+            BookingCancellation? bBancel = GetBookingCancellation(booking);
+            if (bBancel != null)
+            {
+                string message = "Cannot confirm booking : \n" + booking + " \nBecause it has been cancelled by : \n" + bBancel;
+                _logger.LogError(message);
+                throw new Exception(message);
+            }
+            else
+            {
+                BookingConfirmation bc = new BookingConfirmation(new Guid(), booking, new DateTime());
+                FakeData.GetInstance().bookingConfirmations.Add(bc);
+                return bc;
+            }
+        }
+
+        public HotelBooking? GetHotelBooking(Guid HotelBookingId)
+        {
+            return FakeData.GetInstance().hotelBookings?.Where(cb => cb.HotelBookingId == HotelBookingId).FirstOrDefault();
+        }
+
+        public HotelBooking[] GetHotelBookings(Hotel hotel)
+        {
+            return FakeData.GetInstance().hotelBookings.Where(cb => cb.Room.Hotel == hotel).ToArray();
+        }
+
+        public BookingConfirmation? GetBookingConfirmation(HotelBooking booking)
+        {
+            return FakeData.GetInstance().bookingConfirmations.Where(bc => bc.Booking == booking).FirstOrDefault();
+        }
+
+        public BookingCancellation? GetBookingCancellation(HotelBooking booking)
+        {
+            return FakeData.GetInstance().bookingCancellations.Where(bc => bc.Booking == booking).FirstOrDefault();
+        }
+
+        public HotelBooking[] GetHotelBookings(Person guest)
+        {
+            return FakeData.GetInstance().hotelBookings.Where(cb => cb.Guest == guest).ToArray();
+        }
+
+        public Hotel? GetHotel(Guid HotelId)
+        {
+            return FakeData.hotel.Where(hotel => hotel.HotelId == HotelId).FirstOrDefault();
+        }
+
+        public HotelAvailability[] GetHotelAvailabilities(Hotel hotel)
+        {
+            return FakeData.GetInstance().hotelAvailabilities.Where(ca => ca.Hotel == hotel).ToArray();
+        }
+
+        public HotelAvailability AddHotelAvailability(Hotel hotel, DateTime From, DateTime To)
+        {
+            HotelAvailability ca = new HotelAvailability(new Guid(), hotel, From, To);
+            FakeData.GetInstance().hotelAvailabilities.Add(ca);
+            return ca;
+        }
+
     }
 }
