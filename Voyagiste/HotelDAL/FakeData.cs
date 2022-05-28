5b8a666704046ed9eca784cd32595a1cf9c8f7e5
NicolasSt-Arnault <2031116@etu.cegepsth.qc.ca>
2022-05-28 08:26:04 -0400

    Début création des données.

--- Voyagiste/HotelDAL/FakeData.cs
+++ Voyagiste/HotelDAL/FakeData.cs
@@ -1,12 +1,22 @@
-﻿namespace HotelDAL
+﻿using CommonDataDTO;
+using HotelDTO;
+
+namespace HotelDAL
 {
     public class FakeData
     {
         private static FakeData? Singleton;
 
         private FakeData()
-        { 
-        
+        {
+            public record Room(Guid RoomId, Hotel Hotel, string RoomName);
+            public record Hotel(Guid HotelId, Address HotelAddress);
+            public record HotelBooking(Guid HotelBookingId, Room Room, Person Guest, DateTime BookedWhen) : Booking(HotelBookingId, Guest, BookedWhen);
+    public record Address(Guid AddressId, Country country, Region region, City city, PostalCode postalCode, string streetAddress);
+
+            List<Hotel> hotel = new List<Hotel>(new Hotel(new Guid(""), address[0]));
+            new Room(new Guid(""), hotel[0], "Chambre101");
+            new HotelBooking(new Guid(""), country[0], guest[0], new DateTime(2022, 5, 13));
         }
         internal static FakeData GetInstance()
         {
